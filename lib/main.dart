1c768ff3a211c44fe76b7942ff74e8237f9650e3
Fareez Ahamed <fareez.ahamed@gmail.com>
2018-03-10 16:48:30 +0530

    Lap timer working good

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Lap Timer'),
     );
   }
 }
@@ -44,14 +44,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Timer _timer;
+  int _counter = 1;
+  DateTime _currentLapStartTime;
+  Set<Lap> _laps;
 
   _MyHomePageState() {
-    _timer = new Timer.periodic(new Duration(seconds: 1), (timer) {
-      setState(() {
-        _counter++;
-      });
+
+    _laps = new Set<Lap>();
+    _currentLapStartTime = new DateTime.now();
+
+  }
+
+  void completeLap() {
+    setState(() {
+      var _now = new DateTime.now();
+      _laps.add(new Lap(_counter, _now.difference(_currentLapStartTime)));     
+      _currentLapStartTime = _now;
+      _counter++;
     });
   }
 
@@ -80,40 +89,54 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: new ListView(
+        children: _laps.map((lap) => new LapWidget(lap)).toList() 
+        // padding: new EdgeInsets.all(8.0),
+        // itemBuilder: (BuildContext context, int index) {
+        //   return new Text();
+        // },
       ),
       floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: completeLap,
         tooltip: 'Increment',
         child: new Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class Lap {
+  Duration duration;
+  int lapNumber;
+
+  Lap(int lapNumber, Duration duration) {
+    this.lapNumber = lapNumber;
+    this.duration = duration;
+  }
+
+  String lapName() {
+    return "Lap "+lapNumber.toString();
+  }
+
+  String lapTiming() {
+    return duration.toString();
+  }
+}
+
+class LapWidget extends StatelessWidget {
+
+  final Lap _lap;
+
+  LapWidget(this._lap);
+  
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(
+      title: new Text(_lap.lapTiming()),
+      subtitle: new Text(_lap.lapName())
+    );
+  }
+
+
+
 }
